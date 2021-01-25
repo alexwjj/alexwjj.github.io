8f8e8870e3472c8b378016c2a0f4c96700839af7
aleexwjj <wujj@wedoctor.com>
2021-01-25 22:10:08 +0800

    1.25

--- demo.js
+++ demo.js
@@ -1,10 +1,31 @@
-const trottle= (fn, delay=500)=> {
-	let timer = null;
-	return function() {
-		if (timer) return;
-		timer = setTimeout(() => {
-			fn.apply(this, arguments);
-			timer = null;
-		},delay);
+class Sub {
+	constructor() {
+		this.obers = []
+	}
+	add(ob) {
+		this.obers.push(ob)
+	}
+	notify(...args) {
+		this.obers.forEach(ob => ob.update(...args))
+	}
+	remove(ob) {
+		let index = this.obers.indexOf(ob)
+		delete this.obers.splice(index,1)
 	}
 }
+
+class Ob {
+	update(...args) {
+		console.log(...args);
+	}
+}
+
+let ob1 = new Ob()
+let ob2 = new Ob()
+
+let sub = new Sub()
+
+sub.add(ob1)
+sub.add(ob2)
+sub.remove(ob2)
+sub.notify('hhh')
