8f0414ff4c77f9069848e68fdeb2c752935a22bb
aleexwjj <wujj@wedoctor.com>
2021-02-02 21:01:44 +0800

    2.2

--- demo.js
+++ demo.js
@@ -1,52 +1,16 @@
-let p1 = new Promise((resolve, reject) => {
-	setTimeout(() => {
-		resolve('p1');
-	},3000)
-})
-let p2 = new Promise((resolve, reject) => {
-	setTimeout(() => {
-		resolve('p2');
-	},2000)
-})
+function toD (str){
+    let res = str.replace(/[A-Z]/g,function($0){
+        console.log($0,'$0');
+        return `-${$0.toLowerCase()}`;
+    })
+    console.log(res);
+}
+toD('aBcccDeee')
 
-let p3 = new Promise((resolve, reject) => {
-	setTimeout(() => {
-		resolve('p3');
-	},2000)
-})
-Promise.allSettled = function(promises) {
-    return new Promise(function(resolve, reject) {
-        if (!Array.isArray(promises)) {
-            return reject(
-                new TypeError("arguments must be an array")
-            );
-        }
-        let resolvedCounter = 0;
-        const promiseNum = promises.length;
-        // 统计所有的promise结果并最后返回
-        const resolvedResults = new Array(promiseNum);
-        for (let i = 0; i < promiseNum; i++) {
-            Promise.resolve(promises[i]).then(
-                function(value) {
-                    resolvedCounter++;
-                    resolvedResults[i] = value;
-                    if (resolvedCounter == promiseNum) {
-                        return resolve(resolvedResults);
-                    }
-                },
-                // 错误结果
-                function(reason) {
-                    resolvedCounter++;
-                    resolvedResults[i] = reason;
-                    if (resolvedCounter == promiseNum) {
-                        return resolve(reason);
-                    }
-                }
-            );
-        }
-    });
-};
-
-Promise.allSettled([p3,p1,p2]).then(res => {
-	console.log(res);
-})
+function toC (str){
+    let res = str.replace(/-(.)/g,function($0,$1){
+        return $1.toUpperCase();
+    })
+    console.log(res);
+}
+toC('a-bccc-deee')
