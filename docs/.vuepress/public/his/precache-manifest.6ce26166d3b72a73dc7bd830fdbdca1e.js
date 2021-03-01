775073ecbe476c2ff5695b9166b3783b946ee00f
aleexwjj <wujj@wedoctor.com>
2021-03-01 23:27:40 +0800

    his路径修复

---
+++ docs/.vuepress/public/his/precache-manifest.6ce26166d3b72a73dc7bd830fdbdca1e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2c0d217301a46e4e31d58dfd7c838dc",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ee80d4448e5253b3073f",
+    "url": "./static/css/2.191dc5d7.chunk.css"
+  },
+  {
+    "revision": "8b620767e1007ae1059d",
+    "url": "./static/css/main.4bb9f45e.chunk.css"
+  },
+  {
+    "revision": "ee80d4448e5253b3073f",
+    "url": "./static/js/2.1a9f8c56.chunk.js"
+  },
+  {
+    "revision": "00b2142e86c4045bc2e9c7e61fd98dec",
+    "url": "./static/js/2.1a9f8c56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b620767e1007ae1059d",
+    "url": "./static/js/main.1b4d5935.chunk.js"
+  },
+  {
+    "revision": "b9078a807ef73b02d072",
+    "url": "./static/js/runtime-main.3725ae0b.js"
+  },
+  {
+    "revision": "9161e238b798c444b732dbbb7420a09e",
+    "url": "./static/media/404.9161e238.png"
+  },
+  {
+    "revision": "421642f8c4c77139f133d7d1ec05168c",
+    "url": "./static/media/logo.421642f8.png"
+  }
+]);
