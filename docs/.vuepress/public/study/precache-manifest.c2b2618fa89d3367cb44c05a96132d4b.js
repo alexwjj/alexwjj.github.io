c97b64e5da2edf4d5b8fbaf284d3f61204c158c0
wujunjie <wujunjie@youzan.com>
2021-04-07 18:32:25 +0800

    feat: todo demo

---
+++ docs/.vuepress/public/study/precache-manifest.c2b2618fa89d3367cb44c05a96132d4b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f257d39e2af006bad1819077bc6d994",
+    "url": "./index.html"
+  },
+  {
+    "revision": "afbfab3e2b010927b650",
+    "url": "./static/css/2.cd862aec.chunk.css"
+  },
+  {
+    "revision": "360837a565ecd360cfa3",
+    "url": "./static/css/main.2147d5ee.chunk.css"
+  },
+  {
+    "revision": "afbfab3e2b010927b650",
+    "url": "./static/js/2.a066ada6.chunk.js"
+  },
+  {
+    "revision": "38bdadfa33d304ffe0e4df5f5bb2ec8f",
+    "url": "./static/js/2.a066ada6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "360837a565ecd360cfa3",
+    "url": "./static/js/main.203fdde6.chunk.js"
+  },
+  {
+    "revision": "b9078a807ef73b02d072",
+    "url": "./static/js/runtime-main.3725ae0b.js"
+  },
+  {
+    "revision": "9161e238b798c444b732dbbb7420a09e",
+    "url": "./static/media/404.9161e238.png"
+  },
+  {
+    "revision": "421642f8c4c77139f133d7d1ec05168c",
+    "url": "./static/media/logo.421642f8.png"
+  }
+]);
