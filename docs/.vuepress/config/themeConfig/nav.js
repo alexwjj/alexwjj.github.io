4edd5b27328c6391e0b34cc55d640d26a8118fbd
alexwjj <798595965@qq.com>
2023-04-20 10:55:22 +0800

    ai

--- docs/.vuepress/config/themeConfig/nav.js
+++ docs/.vuepress/config/themeConfig/nav.js
@@ -1,8 +1,8 @@
 // nav
 module.exports = [
   { text: '首页', link: '/', icon: 'reco-home' },
-  { 
-    text: '前端', 
+  {
+    text: '前端',
     link: '',
     icon: '',
     items: [
@@ -10,6 +10,7 @@ module.exports = [
         text: '文档教程',
         items: [
           { text: '资源导航', link: '/web/docs/nav/', icon: 'reco-blog' },
+          { text: 'AI', link: '/web/docs/ai/', icon: 'reco-blog' },
           { text: '2022前端面试精华', link: '/web/docs/alexwjj/fe-study/', icon: 'reco-blog' },
           { text: 'ES6 入门教程', link: '/web/docs/es6/', icon: 'reco-blog' },
           { text: '网道-JavaScript 教程', link: '/web/docs/wangdoc/javascript/', icon: 'reco-blog' },
@@ -26,7 +27,7 @@ module.exports = [
       }
     ]
   },
-  
+
   {
     text: '索引',
     icon: 'reco-api',
@@ -37,6 +38,7 @@ module.exports = [
     ]
   },
   { text: '留言板', link: '/bookshop/message-board/', icon: 'reco-suggestion' },
+  { text: 'AI导航', link: '/web/docs/ai/', icon: 'reco-api' },
   { text: '资源导航', link: '/web/docs/nav/', icon: 'reco-api' },
   {
     text: '关于',
@@ -48,13 +50,13 @@ module.exports = [
           { text: 'GitHub', link: 'https://github.com/alexwjj', icon: 'reco-github' },
           { text: '关于我', link: '/bookshop/linkme/', icon: 'reco-account' },
         ]
-      },{
+      }, {
         text: '博客',
         items: [
           { text: '本站源码', link: 'https://github.com/alexwjj/alexwjj.github.io', icon: 'reco-document' },
           { text: '老版博客', link: 'https://alexwjj.github.io/old-blog', icon: 'reco-document' },
         ]
-      },{
+      }, {
         text: '其他',
         items: [
           { text: '友情链接', link: '/bookshop/friendslink/', icon: 'reco-friend' },
